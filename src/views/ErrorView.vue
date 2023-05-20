5e6a4d1593cfbb7f0d7075e034d3c71415cfe996
Agustin Amato <agustin.amato96@gmail.com>
2023-05-20 11:53:21 -0300

    feat: add ErrorView

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>There is an error</h1>
+
+  <div>
+    <button data-test-id="toggle-message" @click="toggleErrorMessage">
+      Toggle more information
+    </button>
+  </div>
+
+  <p v-if="showErrorMessage" data-test-id="message">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, natus? Cumque, similique.
+    Aspernatur quam inventore obcaecati facilis officia unde qui perferendis, in voluptate magni a
+    possimus rerum aliquid saepe corrupti?
+  </p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let showErrorMessage = ref(false)
+
+function toggleErrorMessage() {
+  showErrorMessage.value = !showErrorMessage.value
+}
+</script>
